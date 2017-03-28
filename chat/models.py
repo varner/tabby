44e81a12f5968ea8071e35ff8aef0930028f1645
maddy varner <maddy@badlandsunilimited.com>
2017-03-28 14:55:37 -0400

    moon phases

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+import json
+import urllib2
+
+
+
+def get_moon_phase(month, day, year):
+    # month: int, 1 - 12
+    # day:   int, 1 - 31
+    # year:  int, 1700 - 2100
+    
+    url = "http://api.usno.navy.mil/moon/phase?date=%d/%d/%d&nump=1" % (month, day, year)
+    response = urllib2.urlopen(url)
+    data = json.load(response)
+    phase = data['phasedata'][0]['phase']
+
+    # there's only four phases! we're just converting them into numbers because whatever
+    if   phase == u'New Moon':      return 0
+    elif phase == u'First Quarter': return 1
+    elif phase == u'Full Moon':     return 2
+    elif phase == u'Last Quarter':  return 3
+
+    # if there's a totally different phase then we've really fucked up! return -1 to signal for help
+    return -1
