23963ce6b89b5e74ac33844f05a9514429a72e48
maddy varner <maddy@badlandsunilimited.com>
2017-03-28 17:26:32 -0400

    how do i onboard lol

--- chat/views.py
+++ chat/views.py
@@ -12,7 +12,22 @@ def index(request):
 @twilio_view
 def sms(request):
     # PROCESS TEXT
-
+    # if user exists:
+    username = request.POST.get('From', '')
+    if User.filter(username=username).exists():
+        user = User.objects.get(username=username)
+        if user.get_name():
+            # it's ok to have a conversation
+            pass
+        else:
+            onboard(user) 
     # GIVE EMPTY RESPONSE TO CALLBACK
+    r = Response()
+    return r
+
+def onboard(User):
+    mood = check_mood()
+    ask_for_name()
+    
     r = Response()
     return r
