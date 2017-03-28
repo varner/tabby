dac08e250dbdf8f6eacb67c5572a294133b3f861
maddy varner <maddy@badlandsunilimited.com>
2017-03-28 18:36:01 -0400

    thinking about profiles

--- chat/models.py
+++ chat/models.py
@@ -4,9 +4,32 @@ from django.db import models
 import json
 import urllib2
 
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class RecievedMessage(models.Model):
-    body = models.TextFiled(_("Body"))
+'''
+PROFILE
+'''
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+'''
+RECIEVED MESSAGES
+'''
 
 '''
 def get_moon_phase(month, day, year):
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django_twilio.decorators import twilio_view
+#from django_twilio.decorators import twilio_view
 from twilio.twiml import Response
 
 # Create your views here.
@@ -9,25 +9,28 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 # RECIEVE A NEW TEXT
-@twilio_view
+#@twilio_view
 def sms(request):
     # PROCESS TEXT
     # if user exists:
     username = request.POST.get('From', '')
     if User.filter(username=username).exists():
         user = User.objects.get(username=username)
-        if user.get_name():
-            # it's ok to have a conversation
-            pass
-        else:
-            onboard(user) 
+        # HAVE TO GET THE THING
+        r = Response()
+        return r
+        '''if user.get_name():
+                                    # it's ok to have a conversation
+                                    pass
+                                else:
+                                    onboard(user) '''
     # GIVE EMPTY RESPONSE TO CALLBACK
     r = Response()
     return r
 
-def onboard(User):
+'''def onboard(User):
     mood = check_mood()
     ask_for_name()
-    
+
     r = Response()
-    return r
+    return r'''
