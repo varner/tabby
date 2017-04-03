88f1b2f6d64c17bfe6532acde5265e8df12fe24c
maddy varner <maddy@badlandsunilimited.com>
2017-04-03 19:22:08 -0400

    added tenative message model

--- chat/models.py
+++ chat/models.py
@@ -12,7 +12,7 @@ from django.dispatch import receiver
 PROFILE
 '''
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user  = models.OneToOneField(User, on_delete=models.CASCADE)
     level = models.IntegerField(blank=True, default=0)
     trust = models.IntegerField(blank=True, default=30)
     subscription_end = models.DateField(null=True, blank=True)
@@ -26,28 +26,10 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
-
 '''
 RECIEVED MESSAGES
 '''
-
-'''
-def get_moon_phase(month, day, year):
-    # month: int, 1 - 12
-    # day:   int, 1 - 31
-    # year:  int, 1700 - 2100
-
-    url = "http://api.usno.navy.mil/moon/phase?date=%d/%d/%d&nump=1" % (month, day, year)
-    response = urllib2.urlopen(url)
-    data = json.load(response)
-    phase = data['phasedata'][0]['phase']
-
-    # there's only four phases! we're just converting them into numbers because whatever
-    if   phase == u'New Moon':      return 0
-    elif phase == u'First Quarter': return 1
-    elif phase == u'Full Moon':     return 2
-    elif phase == u'Last Quarter':  return 3
-
-    # if there's a totally different phase then we've really fucked up! return -1 to signal for help
-    return -1
-'''
+class Message(models.Model):
+    sender       = models.ForeignKey(User, related_name="sender")
+    last_updated = models.DateField(null=True, blank=True)
+    content      = models.TextField(null=True, blank=True)
