f2d74fe9cfd07e3de418dad6ef93b93b78a05c1f
maddy varner <maddy@badlandsunilimited.com>
2017-04-04 13:45:41 -0400

    url pattern fix

--- taboo/urls.py
+++ taboo/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^chat/', include('chat.urls')),
+	url(r'^/', include('chat.urls')),
     url(r'^admin/', admin.site.urls),
 ]
