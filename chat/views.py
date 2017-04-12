23effadd644c99d2bbcbc7c2932676f25586a7f7
Madeleine Varner <maddyvarner@gmail.com>
2017-04-12 15:40:28 -0400

    trying this profile function out

--- chat/views.py
+++ chat/views.py
@@ -26,17 +26,18 @@ def sms(request):
         print "user exists"
         recieved_message = request.POST.get('Body', '')
         user = User.objects.get(username=username)
-        # IF MESSAGE ALREADY EXISTS
-        if Message.objects.filter(sender=user).exists():
-            # APPEND MESSAGE TO MESSAGE QUEUE
-            message = Message.objects.get(sender=user)
-            message.body += "\n%s" % recieved_message
-            message.last_updated = timezone.now()
-            message.save()
-        else: # ELSE MAKE MESSAGE
-            Message.objects.create_message(sender=user, body=recieved_message)
-            # THROW RESPONSE INTO QUEUE
-        # GIVE EMPTY RESPONSE TO CALLBACK
-        return HttpResponse(r.toxml(), content_type='text/xml')
+        if user.Profile.isActive():
+            # IF MESSAGE ALREADY EXISTS
+            if Message.objects.filter(sender=user).exists():
+                # APPEND MESSAGE TO MESSAGE QUEUE
+                message = Message.objects.get(sender=user)
+                message.body += "\n%s" % recieved_message
+                message.last_updated = timezone.now()
+                message.save()
+            else: # ELSE MAKE MESSAGE
+                Message.objects.create_message(sender=user, body=recieved_message)
+                # THROW RESPONSE INTO QUEUE
+            # GIVE EMPTY RESPONSE TO CALLBACK
+            return HttpResponse(r.toxml(), content_type='text/xml')
     # GIVE EMPTY RESPONSE TO CALLBACK
     return HttpResponse(r.toxml(), content_type='text/xml')
