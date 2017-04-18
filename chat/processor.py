4e4f77b65b23992798e93cded9a8db32dcf94a03
maddy varner <maddy@badlandsunilimited.com>
2017-04-18 17:02:30 -0400

    adding logs

--- chat/processor.py
+++ chat/processor.py
@@ -57,16 +57,17 @@ def send_message(username, message):
 def collect_messages(last_checked):
     accessed_callers = set()
 
-    for message in twilio_client.messages.list(date_sent=last_checked.date()):
-        phone = message.from_
+    for text in twilio_client.messages.list(date_sent=last_checked.date()):
+        phone = text.from_
         if (phone in accessed_callers or Caller.objects.filter(phone=phone).exists()):
             caller = Caller.objects.get(phone=phone)
-            date_sent = message.date_sent.replace(tzinfo=pytz.utc)
-            logging.error(date_sent, message.body)
+            date_sent = text.date_sent.replace(tzinfo=pytz.utc)
+            logging.error(date_sent)
+            logging.error(text.body)
             logging.error(last_checked)
             if date_sent >= last_checked:
                 if caller.isActive():
-                    recieved_message = message.body
+                    recieved_message = text.body
                     # IF MESSAGE ALREADY EXISTS
                     if Message.objects.filter(sender=caller).exists():
                         # APPEND MESSAGE TO MESSAGE QUEUE
