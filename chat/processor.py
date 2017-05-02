08062110366021b3ffb690ae6cb7a0673cb869b5
maddy varner <maddy@badlandsunilimited.com>
2017-05-02 17:57:49 -0400

    added freetime check

--- chat/processor.py
+++ chat/processor.py
@@ -56,14 +56,11 @@ def send_message(username, message):
 def during_freetime():
     schedule = [{ "start": datetime.time(7, 30, 0),
       "end": datetime.time(10, 30, 0)
-    }, 
-    { "start": datetime.time(13, 0, 0),
+    }, { "start": datetime.time(13, 0, 0),
       "end": datetime.time(17, 20, 0)
-    },
-    { "start": datetime.time(19, 11, 0),
+    }, { "start": datetime.time(19, 11, 0),
       "end": datetime.time(19, 55, 0)
-    },
-    { "start": datetime.time(20, 5, 0),
+    }, { "start": datetime.time(20, 5, 0),
       "end": datetime.time(23, 3, 20)
     }]
 
